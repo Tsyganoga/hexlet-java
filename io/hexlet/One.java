49bb08549b37a88c2dbd0b7a557ca5d79accff49
Natalia Tsyganova <tsyganova.nn@gmail.com>
2022-11-23 21:02:45 +0300

    add One.class

--- io/hexlet/One.java
+++ io/hexlet/One.java
@@ -1,6 +1,22 @@
 package io.hexlet;
+
 public class One {
     public static void main(String[] args) {
-        Two.greeting();
+        String s = "Hexlet";
+        if(s.length() > 0) {
+            reverseString(s, s.length() - 1);
+        }
+    } 
+        
+     public static void reverseString(String s, int index) {
+        if(index == 0) {
+            System.out.println(s.charAt(index));     
+     
+	    return;
+        }
+		        
+        System.out.print(s.charAt(index));
+				        
+        reverseString(s, index - 1);
     }
-}
+}	   
